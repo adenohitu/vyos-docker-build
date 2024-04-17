f20a4a92d62d56f18e59f5f929dc8171b0f9a1f9
ikuta-hk <r.ikuta@hakoreco.com>
2024-04-17 17:05:43 +0900

    init: vyos build docker

---
+++ build.sh
@@ -0,0 +1,9 @@
+mkdir vyos && cd vyos
+curl -o vyos-1.5-rolling-202404141045-amd64.iso https://github.com/vyos/vyos-rolling-nightly-builds/releases/download/1.5-rolling-202404141045/vyos-1.5-rolling-202404141045-amd64.iso
+mkdir rootfs
+sudo mount -o loop vyos-1.5-rolling-202404141045-amd64.iso rootfs
+sudo apt-get install -y squashfs-tools
+mkdir unsquashfs
+sudo unsquashfs -f -d unsquashfs/ rootfs/live/filesystem.squashfs
+sudo tar -C unsquashfs -c . | docker import - vyos:1.5-rolling-202404141045
+sudo umount rootfs
